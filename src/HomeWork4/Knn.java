1924edde618461682860e224c59174eacf9c3441
Hilla Barkal <hilla.guz@gmail.com>
2017-05-26 19:34:45 +0300

    calcConfusion stuff

--- src/HomeWork4/Knn.java
+++ src/HomeWork4/Knn.java
@@ -10,29 +10,32 @@ import weka.core.Instance;
 import weka.core.Instances;
 
 public class Knn implements Classifier {
-	
-	public enum EditMode {None, Forwards, Backwards};
+
+	public enum EditMode {
+		None, Forwards, Backwards
+	};
+
 	private EditMode m_editMode = EditMode.None;
 	private Instances m_trainingInstances;
 	private int m_k; // {1, 2, …, 20}
 	private int m_p; // {infinity(0), 1, 2, 3}
 	private String m_majority; // {"uniform", "weighted"}
-	
-	private class Neighbor implements Comparable<Neighbor>{
+
+	private class Neighbor implements Comparable<Neighbor> {
 		public Instance instance;
 		public double distance;
-		
-		public Neighbor(Instance instance, double distance){
+
+		public Neighbor(Instance instance, double distance) {
 			this.instance = instance;
 			this.distance = distance;
 		}
-		
+
 		@Override
 		public int compareTo(Neighbor neighbor) {
 			double diff = this.distance - neighbor.distance;
-			if (diff < 0){
+			if (diff < 0) {
 				return -1;
-			} else if (diff > 0){
+			} else if (diff > 0) {
 				return 1;
 			}
 			return 0;
@@ -46,10 +49,10 @@ public class Knn implements Classifier {
 	public void setEditMode(EditMode editMode) {
 		m_editMode = editMode;
 	}
-	
+
 	/**
-	 * Builds a kNN from the training data. The method is already implemented 
-	 * using switch statement on the enum EditMode. This enum set the edit mode 
+	 * Builds a kNN from the training data. The method is already implemented
+	 * using switch statement on the enum EditMode. This enum set the edit mode
 	 * to one of its possibilities (None, Forwards, Backwards).
 	 */
 	@Override
@@ -68,13 +71,15 @@ public class Knn implements Classifier {
 			noEdit(arg0);
 			break;
 		}
-		/* You should implement each one of the helper methods noEdit, 
-		 * editedForward and editedBackward (the last 2 describe later). */
+		/*
+		 * You should implement each one of the helper methods noEdit,
+		 * editedForward and editedBackward (the last 2 describe later).
+		 */
 	}
 
 	@Override
 	public double classifyInstance(Instance instance) {
-		//TODO: implement this method
+		// TODO: implement this method
 		return 0;
 	}
 
@@ -84,7 +89,7 @@ public class Knn implements Classifier {
 			data.remove(x);
 			m_trainingInstances = data;
 			if (classifyInstance(x) != x.classValue()) {
-				// if x is classify correctly by instances = {x}, 
+				// if x is classify correctly by instances = {x},
 				// remove x from instances. else - return x to instances!
 				data.add(x);
 			}
@@ -98,7 +103,7 @@ public class Knn implements Classifier {
 			data.remove(x);
 			m_trainingInstances = data;
 			if (classifyInstance(x) == x.classValue()) {
-				// if x is NOT classify correctly by instances = {x}, 
+				// if x is NOT classify correctly by instances = {x},
 				// add x to instances. else - remove x from instances!
 				data.add(x);
 			}
@@ -108,6 +113,7 @@ public class Knn implements Classifier {
 
 	/**
 	 * Store the training set in the m_trainingInstances without editing.
+	 * 
 	 * @param instances
 	 */
 	private void noEdit(Instances instances) {
@@ -125,85 +131,110 @@ public class Knn implements Classifier {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/**
-	 * Calculate the average error on a given instances set. The average error is 
-	 * the total number of classification mistakes on the input instances set and 
-	 * divides that by the number of instances in the input set
+	 * Calculate the average error on a given instances set. The average error
+	 * is the total number of classification mistakes on the input instances set
+	 * and divides that by the number of instances in the input set
+	 * 
 	 * @param instace
-	 * @return Average error (double). 
+	 * @return Average error (double).
 	 */
 	private double calcAvgError(Instances instances) {
 		int numOfInstances = instances.numInstances();
-		int numOfMistakes= 0;
+		int numOfMistakes = 0;
 		Instance instance;
-		
+
 		// goes through each instances and check the rediction is
 		// it's actual classification, otherwise counts to mistakes
-		for (int i = 0; i < numOfInstances; i++){
+		for (int i = 0; i < numOfInstances; i++) {
 			instance = instances.instance(i);
-			if (instance.classValue() != classifyInstance(instance)){
+			if (instance.classValue() != classifyInstance(instance)) {
 				numOfMistakes++;
 			}
 		}
-		
+
 		// calculates the average error...
-		double avgError = numOfMistakes / (double) numOfInstances; 
-		
+		double avgError = numOfMistakes / (double) numOfInstances;
+
 		return avgError;
 	}
-	
+
 	/**
 	 * Calculate the Precision & Recall on a given instances set.
+	 * 
 	 * @param instances
-	 * @return double array of size 2. First index for Precision and the second 
-	 * for Recall.
+	 * @return double array of size 2. First index for Precision and the second
+	 *         for Recall.
 	 */
 	private double[] calcConfusion(Instances instances) {
-		double truePositive = 0;
-		double falsePositive = 0;
-		double falseNegative = 0;
-		return null;
+		double truePositive = 0; // prediction positive and condition positive 
+		double falsePositive = 0; // prediction positive and condition negative
+		double falseNegative = 0; // prediction negative and condition positive
+		
+		// count population
+		for (Instance instance : instances) {
+			boolean conditionPositive = instance.classValue() == 1;
+			boolean predictionPositive = classifyInstance(instance) == instance.classValue();
+			if (predictionPositive && conditionPositive) {
+				truePositive++;
+			} else if (predictionPositive && !conditionPositive) {
+				falsePositive++;
+			} else if (!predictionPositive && conditionPositive) {
+				falseNegative++;
+			}
+		}
+		
+		// calculate precision and recall
+		double precision = truePositive / (truePositive + falsePositive);
+		double recall = truePositive / (truePositive + falseNegative);
+		double[] ret = {precision, recall};
+		return ret;
 	}
-	
+
 	/**
 	 * Calculate the cross validation error = average error on all folds.
+	 * 
 	 * @param instances
 	 * @return Average fold error (double)
 	 */
 	private double crossValidationError(Instances instances) {
 		return 0;
 	}
-	
+
 	/**
 	 * Find the K nearest neighbors for the instance being classified.
+	 * 
 	 * @param instance
-	 * @return K nearest neighbors and their distances 
+	 * @return K nearest neighbors and their distances
 	 */
 	private ArrayList<Neighbor> findNearestNeighbors(Instance instance) {
 		int numOfTrainingInstances = m_trainingInstances.size();
-		Neighbor[] neighbors = new Neighbor[numOfTrainingInstances]; 
-		
+		Neighbor[] neighbors = new Neighbor[numOfTrainingInstances];
+
 		// array of all instances and their distance from the given instance
-		for (int i = 0; i < numOfTrainingInstances; i++){
+		for (int i = 0; i < numOfTrainingInstances; i++) {
 			neighbors[i].instance = m_trainingInstances.get(i);
 			neighbors[i].distance = distance(neighbors[i].instance, instance);
 		}
 		// convert the array of neighbors to an array list (in order to compare)
 		ArrayList<Neighbor> neighborsList = new ArrayList<Neighbor>(Arrays.asList(neighbors));
-		
+
 		// sorts the array list of neighbors
 		Collections.sort(neighborsList);
-		
-		// takes only the first (smallest) k neighbors and puts in a new array list 
+
+		// takes only the first (smallest) k neighbors and puts in a new array
+		// list
 		ArrayList<Neighbor> kNN = new ArrayList<>(neighborsList.subList(0, m_k - 1));
-		
+
 		return kNN;
 	}
-	
+
 	/**
 	 * Calculate the majority class of the neighbors.
-	 * @param neighbors - a set of K nearest neighbors
+	 * 
+	 * @param neighbors
+	 *            - a set of K nearest neighbors
 	 * @return the majority vote on the class of the neighbors
 	 */
 	private double getClassVoteResult(ArrayList<Neighbor> neighbors) {
@@ -216,35 +247,40 @@ public class Knn implements Classifier {
 		}
 		return 1;
 	}
-	
+
 	/**
-	 * Calculate the weighted majority class of the neighbors. In this method 
-	 * the class vote is normalized by the distance from the instance being 
-	 * classified. Instead of giving one vote to every class, you give a vote 
-	 * of 1/(distance from instance)^2 
-	 * @param neighbors - set of K nearest neighbors (and perhaps their distances)
-	 * @return the majority vote on the class of the neighbors, where each 
-	 * neighbor's class is weighted by the neighbor’s distance from the 
-	 * instance being classified.
+	 * Calculate the weighted majority class of the neighbors. In this method
+	 * the class vote is normalized by the distance from the instance being
+	 * classified. Instead of giving one vote to every class, you give a vote of
+	 * 1/(distance from instance)^2
+	 * 
+	 * @param neighbors
+	 *            - set of K nearest neighbors (and perhaps their distances)
+	 * @return the majority vote on the class of the neighbors, where each
+	 *         neighbor's class is weighted by the neighbor’s distance from the
+	 *         instance being classified.
 	 */
 	private double getWeightedClassVoteResult(ArrayList<Neighbor> neighbors) {
 		double[] countClassifications = new double[2];
 		for (Neighbor inst : neighbors) {
 			double vote = 1.0 / Math.pow(inst.distance, 2);
-			countClassifications[(int) inst.instance.classValue()] += vote; 
+			countClassifications[(int) inst.instance.classValue()] += vote;
 		}
 		if (countClassifications[0] > countClassifications[1]) {
 			return 0;
 		}
 		return 1;
 	}
-	
+
 	/**
 	 * Calculate distance between two instances
-	 * @param first instance
-	 * @param second instance
-	 * @return input instances’ distance according to the distance function that 
-	 * your algorithm is configured to use.
+	 * 
+	 * @param first
+	 *            instance
+	 * @param second
+	 *            instance
+	 * @return input instances’ distance according to the distance function that
+	 *         your algorithm is configured to use.
 	 */
 	private double distance(Instance first, Instance second) {
 		if (m_p == 0) {
@@ -253,13 +289,16 @@ public class Knn implements Classifier {
 			return lpDistance(first, second);
 		}
 	}
-	
+
 	/**
 	 * Calculate l-p distance between two instances
-	 * @param first instance
-	 * @param second instance
-	 * @return the l-p distance between the two instances
-	 * note: p can be a variable of your class or you can set p some other way.
+	 * 
+	 * @param first
+	 *            instance
+	 * @param second
+	 *            instance
+	 * @return the l-p distance between the two instances note: p can be a
+	 *         variable of your class or you can set p some other way.
 	 */
 	private double lpDistance(Instance first, Instance second) {
 		double distance = 0;
@@ -271,11 +310,14 @@ public class Knn implements Classifier {
 		}
 		return Math.pow(distance, 1.0 / m_p);
 	}
-	
+
 	/**
 	 * Calculate l-infinity distance between two instances
-	 * @param first instance
-	 * @param second instance
+	 * 
+	 * @param first
+	 *            instance
+	 * @param second
+	 *            instance
 	 * @return the l-infinity distance between two instances
 	 */
 	private double lInfinityDistance(Instance first, Instance second) {
